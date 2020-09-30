56c7b97910209e7b926e623481f6d9ece582cbae
konghub <kong@gmail.pornhub>
2020-09-30 21:40:30 +0700

    logo

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,6 +1,6 @@
  <!-- Nav -->
 
- <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" >
    <div class="container">
      <a class="navbar-brand" href="#"></a>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,21 +8,24 @@
      </button>
      <div class="collapse navbar-collapse" id="navbarText">
        <ul class="navbar-nav mr-auto">
-         <li class="nav-item">
-           <a class="nav-link" href="<?php echo base_url(); ?>index.php/mag_login/list">รายชื่อศิษย์เก่า<span class="sr-only">(current)</span></a>
-         </li>
+           <span class="navbar-text">
+             <img src="<?php echo base_url('img/l4.png') ?>" alt="" srcset="" width="50px" height="40px"></span>
+             <span class="navbar-text">
+             <a class="nav-link" href="<?php echo base_url(); ?>index.php/mag_login/list">รายชื่อศิษย์เก่า<span class="sr-only">(current)</span></a>
+            </span>
+           </div>
        </ul>
        <?php if ($this->session->userdata('email')) { ?>
          <!-- Split button -->
          <div class="btn-group">
-           <button type="button" class="btn btn-deep-orange darken-3 btn-rounded waves-effect"data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-biking"></i><?php echo $this->session->userdata('fname') ?><?php echo nbs(3) ?></button>
-          
+           <button type="button" class="btn btn-deep-orange darken-3 btn-rounded waves-effect" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-biking"></i><?php echo $this->session->userdata('fname') ?><?php echo nbs(3) ?></button>
+
            <div class="dropdown-menu">
              <a class="dropdown-item" href="<?php echo base_url('index.php/welcome/profile'); ?>">ข้อมูลส่วนตัว</a>
              <a class="dropdown-item" href="<?php echo base_url('index.php/mag_login/logout'); ?>">ออกจากระบบ</a>
-             
+
            </div>
-       </div>
+         </div>
        <?php } else { ?>
          <span class="navbar-text">
            <a class="nav-link" href="<?php echo base_url(); ?>index.php/welcome/regis">ลงทะเบียนศิษย์เก่า</a>
